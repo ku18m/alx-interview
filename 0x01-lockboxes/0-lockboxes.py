231b7b38a81ad3740db784a6da32208ebb58138e
Mohamed Kamal <ku18mz@gmail.com>
2024-05-02 20:54:54 +0300

    update contiotion for empty boxes, thanks to Codium

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,4 +13,4 @@ def canUnlockAll(boxes):
                 if new_key not in keys and new_key < len(boxes):
                     keys.append(new_key)
 
-    return len(keys) == len(boxes)
+    return (len(keys) == len(boxes) or len(boxes) == 0)
