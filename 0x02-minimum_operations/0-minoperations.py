2a441f83258a0e48ddb25486ac22b3e02921d970
Mohamed Kamal <ku18mz@gmail.com>
2024-05-10 04:26:47 +0300

    fix pycs error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,8 @@
 
 
 def minOperations(n: int) -> int:
-    """Returns the minimum number of operations needed to result in exactly n H characters in the file"""
+    """Returns the minimum number of operations needed,
+    to result in exactly n H characters in the file"""
     if n <= 1:
         return 0
     for i in range(2, n + 1):
